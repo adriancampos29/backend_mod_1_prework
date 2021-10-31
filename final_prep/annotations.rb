4bd750b337a1ca524d2aaff5f8239bf6b60bee58
Adrian Campos <adriancampos2992@yahoo.com>
2021-10-30 19:16:28 -0700

    add final prep

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -1,43 +1,71 @@
-# Add your annotations, line by line, to the code below using code comments.
+# Add your annotations, line by line, to the code blow using code comments.
 # Try to focus on using correct technical vocabulary.
 # Use the # to create a new comment
 
-# Build a Bear
-
+# Defining a method which is build a bear, with variables inside the parathesis that are given with a command
 def build_a_bear(name, age, fur, clothes, special_power)
+  # Setting variable greeting to a string with interpolation 
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
+  # setting demographics variable to an array with name and age inside
   demographics = [name, age]
+  # Setting variable power saying with a string that has interpolation
   power_saying = "Did you know that I can #{special_power}?"
+  # Builidng a hash with built_bear as the name of it
   built_bear = {
+    # Basic info is the variable name with demographics being its string value demographics
     'basic_info' => demographics,
+    # Clothes variable set to string value clothes
     'clothes' => clothes,
+    # exterior variable set to string value fur
     'exterior' => fur,
+    # cost variable set to float value 49.99
     'cost' => 49.99,
+    # saying variable is set to an array consisting of two variables and one string value
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
+    # is cuddly variable set to boolean value true
     'is_cuddly' => true,
   }
+  # Stores built_bear hash
   return built_bear
+  # ends the function
 end
 
+# Calling the build a bear hash, it is now going to store these variables and functions intho the original 5 that it was given. For example, the fluffy variable will go into the name variable, the 4 interger will go into the age variable, the brown variable will go into fur variable.
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
+
+# Calling the build a bear hash, it will now store these variables into the original 5 that were given. For example, Sleeply will go into the name variable, the integer 2 will go into the age variable, the variable purple will go into the fur variable.
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
-# FizzBuzz
-
+# Defining method fizzbuzz with 3 variables stored in it, num_1, num_2, and range
 def fizzbuzz(num_1, num_2, range)
+  # 1..range will compute from the numer 1 to the variable range. .each loops this varaible while do tells it to loop. The |i| is place holder for indexes
   (1..range).each do |i|
+    # The if statemenet is stating if i has a remindar equal to num_1 that is equal to 0 AND if i has a remindar equal to num_2 that is equal to zero
     if i % num_1 === 0 && i % num_2 === 0
+      # print fizzbuzz if the given condition above is true
       puts 'fizzbuzz'
+      # elsif is stating that if the first condition isn't met, then print this method. This elsif method states if i has a remindar that is equal to num_1 that is equal to 0
     elsif i % num_1 === 0
+      # Print fizz if the given condition above is true
       puts 'fizz'
-    elsif i % num_2 === 0
+      # elsif is stating that if the given if statement is false, then go to the next elsif statement. This is stating if i has a remindar equal to num_2 that is equal to=
+      elsif i % num_2 === 0
+        # Print buzz if the given condition above is true
       puts 'buzz'
+      # else is the final condition in a if, elsif statement. If the if and elsif statements are false, else will automatically be the final condition.
     else
+      # print i if the given conditions of if, elsif are false
       puts i
+      # ends the if, elsif, else fuctions
     end
+    # ends the looping function
   end
+  # ends the method function
 end
 
+# prints fizzbuzz with the given array in the index we defined earlier, that is i. This will also follow the conditions we gave it earlier, meaning the looping, if, elsif, and else functions.
 fizzbuzz(3, 5, 100)
-fizzbuzz(5, 8, 400)
+
+# prints fizzbuzz with the given array in the index we defined earlier, that is i. This will also follow the given condtions we gave it earlier meaning the loopings, if, elsif, and else functions.
+fizzbuzz(5,8,400)
--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -2,44 +2,87 @@
 
 # Declare two variables - hero_name AND special_ability - set to strings
 
+hero_name = "Spiderman"
+special_ability = "spidey sense"
+
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
 
+greeting = "Hello! I am #{hero_name}!"
+catchphrase = "I have the ability #{special_ability} that allows me to sense danger!"
+
+
 # Declare two variables - power AND energy - set to integers
+
+power = 100
+energy = 110
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
 
+full_power = power * 500
+full_energy = energy + 150
+
 # Declare two variables - is_human and identity_concealed - assigned to booleans
 
+is_human = true
+identity_concealed = true
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
 
+arch_enemies = ['Green Goblin', 'Doc Oct', 'Sandman']
+sidekicks = ['Captain America', 'Iron Man', 'Thor']
+
 # Print the first sidekick to your terminal
+
+print sidekicks.first
 
 # Print the last arch_enemy to the terminal
 
+print arch_enemies.last
+
 # Write some code to add a new arch_enemy to the arch_enemies array
+
+arch_enemies.append('Venom')
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
 
+print arch_enemies
+
 # Remove the first sidekick from the sidekicks array
+
+sidekicks.shift
 
 # Print the sidekicks array to terminal to ensure you added a new sidekick
 
+print sidekicks
+
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
+
+def assess_situation(danger_level, save_the_day, bad_excuse)
+ if danger_level > 50
+    print  "#{bad_excuse}"
+  elsif danger_level = 10..50
+    print "#{save_the_day}"
+  elsif danger_level < 10
+    print "meh. hard pass."
+  end
+end
+
+assess_situation(20, "We did it!", "I gotta go!")
+
 
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -55,17 +98,29 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
-
+scary_monster = {
+  'name' => 'MOUSEMAN',
+  'smell' => 'sewer',
+  'weight' => 500,
+  'citiesDestroyed' => ['Bakersfield', 'Los Angeles', 'Wasco'],
+  'luckyNumbers' => [14, 29, 111]
+},
+address = {
+  'number' => 2212,
+  'street' => 'Bay Club Court',
+  'state' => 'California',
+  'zip' => 93303
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
@@ -75,10 +130,44 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 # - Create 2 instances of your SuperHero class
 
 
+class Superhero
+attr_accessor :name, :super_power, :age
+  attr_reader :arch_nemesis, :power_level, :energy_level
+def initialize(arch_nemesis, power_level, energy_level)
+ @arch_nemesis = "The Syntax Error"
+@power_level = 100
+@energy_level = 50
+end
+
+def say_name(name)
+  puts name
+end
+
+def maximize_energy
+  @energy_level = 1000
+  puts "Your energy is #{@energy_level} now!"
+end
+
+def gain_power(power)
+  @power_level += power
+  puts "You gained #{power} more power"
+end
+end
+
+hero1 = Superhero.new("landman", 40, 50)
+hero2 = Superhero.new("garbageman", 100, 200)
+
+puts hero1.gain_power(20)
+puts hero2.gain_power(100)
+puts hero1.maximize_energy
+puts hero1.say_name('aqua man')
+
 # Reflection
 # What parts were most difficult about this exerise?
+#1).  The most difficult part of this exercise was class/methods for me. I really need to go back and reread and rework each individual section. I felt like I was just guessing and trying every little thing until it worked and not understanding how it works. This is something I will study heavily going into mod 1 as I feel i'm not as prepared as I need to be. I want to be able to have it down.
 
 # What parts felt most comfortable to you?
+#2). I woulsd say section 1 and 3 I have the best grasp on. Arrays and hashes I can visulize and dont need to keep plugging in different things to see if my solution is coreect. However section 2 and 4 I will redo the entire portions as I dont have the grasp I would like just yet. I need to understand the ins and outs of those because I know I didnt grasp the concepts well enought to be comfortable explaining them. Espcially section 4 with the classes and methods. This was the thing I had most trouble with.
 
 # What skills do you need to continue to practice before starting Mod 1?
-
+#3). The skills I need to continue to practice are for sure anytning given in section 2 and 4. This includes If, elsif, and else statements. I understand them but want a better grasp. Section 4 is something I will revist and redo in its entirely as I know im not comfortable with it just yet. 
